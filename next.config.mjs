04c007e3621d3ea18e69e14d9b08e3882dd11009
yelnurdo <yelnurdos@gmail.com>
2024-07-05 06:16:46 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['localhost'],
+      domains: ['localhost', 'fortunebackend-production.up.railway.app'],
     },
   };
   
